e3af0eaa38c651340c6b84a1975c21e0a43bf610
ZkPrj <zk_sima@163.com>
2016-04-12 23:48:52 +0800

    测试用

---
+++ ZKAddrList/ZKAddrList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZKAddrList
+//
+//  Created by 陈婷 on 16/4/12.
+//  Copyright © 2016年 zk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZKAddrList/ZKAddrList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZKAddrList
+//
+//  Created by 陈婷 on 16/4/12.
+//  Copyright © 2016年 zk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
